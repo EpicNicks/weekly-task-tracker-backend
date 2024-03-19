c378add9a0bed72b3b4ce3f901b4cdbc184f9471
Nicholas Cerisano <epicnicks2.0@gmail.com>
2024-03-18 22:52:43 -0400

    updated db models to not use timestamps

--- src/models/DailyLog.ts
+++ src/models/DailyLog.ts
@@ -3,7 +3,10 @@ import Task from './Task'
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    define: {
+        timestamps: false,
+    },
 })
 
 export default class DailyLog extends Model<InferAttributes<DailyLog>, InferCreationAttributes<DailyLog>> {
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,10 @@ import bcrypt from 'bcrypt'
 
 const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    define: {
+        timestamps: false,
+    },
 })
 
 export default class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
